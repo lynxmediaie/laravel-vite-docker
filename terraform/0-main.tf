8638d444973b235b018f910c4cd9552babe8b12f
Marek <marek@culak.sk>
2025-02-06 11:08:43 +0000

    path to work dir

--- terraform/0-main.tf
+++ terraform/0-main.tf
@@ -12,7 +12,7 @@ terraform {
     }
   }
   backend "local" {
-    path = "/tmp/runner/work/laravel-vite-docker/laravel-vite-docker/terraform"
+    # path = "/tmp/runner/work/laravel-vite-docker/laravel-vite-docker/terraform"
     workspace_dir = "/tmp/runner/work/laravel-vite-docker/laravel-vite-docker/terraform"
   }
 }
